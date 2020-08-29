38b59d42a99b1eea8a545f4930e7670a04257448
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-08-29 12:38:20 +0500

    sliding window pattern

---
+++ src/patterns/slidingWindow.js
@@ -0,0 +1,29 @@
+/**
+ * Sliding Window pattern is useful for finding or analyzing a subset of continuos data from a string or array
+ */
+
+/**
+ * Find the maximum sum of of n consecutive elements in the array (arr)
+ * @param {number[]} arr array of numbers
+ * @param {number} n sub array length
+ * @returns sum
+ */
+const maxSubArraySum = (arr, n) => {
+  if (n === 0 || n > arr.length) return;
+
+  let sum = arr.slice(0, n).reduce((n1, n2) => n1 + n2, 0); // window
+  let highest = sum;
+  let i = 0; // i = start j = end
+
+  for (let j = n; j < arr.length; j++) {
+    sum = sum - arr[i] + arr[j]; // sliding
+    i++;
+    if (sum > highest) highest = sum;
+  }
+
+  return highest;
+};
+
+module.exports = {
+  maxSubArraySum,
+};
---
+++ src/patterns/slidingWindow.test.js
@@ -0,0 +1,21 @@
+const { maxSubArraySum } = require("./slidingWindow");
+
+test("max sub array sum - invalid", () => {
+  expect(maxSubArraySum([1], 0)).toBeUndefined();
+  expect(maxSubArraySum([1, 2], 10)).toBeUndefined();
+});
+
+test("max sub array sum - simple", () => {
+  expect(maxSubArraySum([1], 1)).toBe(1);
+  expect(maxSubArraySum([1, 2], 1)).toBe(2);
+  expect(maxSubArraySum([2, 1], 1)).toBe(2);
+});
+
+test("max sub array sum - complex", () => {
+  expect(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2)).toBe(10);
+  expect(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 4)).toBe(17);
+  expect(maxSubArraySum([4, 2, 1, 6], 1)).toBe(6);
+  expect(maxSubArraySum([4, 2, 1, 6], 4)).toBe(13);
+  expect(maxSubArraySum([-1, -2, -3, -1, 0], 2)).toBe(-1);
+  expect(maxSubArraySum([], 4)).toBeUndefined();
+});
