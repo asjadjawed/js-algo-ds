4a10c5ecf159ad4bdaa9674334ba77a62b838f3f
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-08-31 13:09:22 +0500

    binary search

---
+++ src/searching/binarySearch.js
@@ -0,0 +1,23 @@
+/**
+ * Binary search for value in sorted array
+ * @param {number[]} arr
+ * @param {number} value
+ */
+const binarySearch = (arr, value) => {
+  let left = 0;
+  let right = arr.length - 1;
+  let check = Math.floor((left + right) / 2);
+
+  while (left <= right) {
+    if (arr[check] === value) return check;
+    else if (arr[check] > value) right = check - 1;
+    else left = check + 1;
+    check = Math.floor((left + right) / 2);
+  }
+
+  return -1;
+};
+
+module.exports = {
+  binarySearch,
+};
---
+++ src/searching/binarySearch.test.js
@@ -0,0 +1,15 @@
+const { binarySearch } = require("./binarySearch");
+
+test("binary search for value in sorted array", () => {
+  expect(binarySearch([], 10)).toBe(-1);
+  expect(binarySearch([10], 10)).toBe(0);
+  expect(binarySearch([1, 2, 3, 4, 5, 6], 10)).toBe(-1);
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6], 3)).toBe(3);
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6], 2)).toBe(2);
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6], 5)).toBe(5);
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6], 0)).toBe(0);
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6], 6)).toBe(6);
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6], -100)).toBe(-1);
+  expect(binarySearch([0, 1, 2, 3, 4, 5, 6], 100)).toBe(-1);
+  expect(binarySearch([1, 2, 34, 56], 56)).toBe(3);
+});
