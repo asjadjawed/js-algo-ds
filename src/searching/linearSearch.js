bd2c408bc579109209506905d566394cff731d2f
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-08-31 12:13:23 +0500

    linear search

---
+++ src/searching/linearSearch.js
@@ -0,0 +1,15 @@
+/**
+ * Linear search for array using value
+ * @param {number[]} arr
+ * @param {number} value
+ */
+const linearSearch = (arr, value) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) return i;
+  }
+  return -1;
+};
+
+module.exports = {
+  linearSearch,
+};
---
+++ src/searching/linearSearch.test.js
@@ -0,0 +1,9 @@
+const { linearSearch } = require("./linearSearch");
+
+test("linear search", () => {
+  expect(linearSearch([], 10)).toBe(-1);
+  expect(linearSearch([10], 10)).toBe(0);
+  expect(linearSearch([1, 2, 3, 4, 5, 6], 10)).toBe(-1);
+  expect(linearSearch([1, 2, 3, 4, 5, 6], 3)).toBe(2);
+  expect(linearSearch([34, 56, 1, 2], 1)).toBe(2);
+});
