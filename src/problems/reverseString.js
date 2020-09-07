341713c288ecc824ca002e8a19e1a9e05ada7759
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-09-07 15:15:19 +0500

    string reversal

---
+++ src/problems/reverseString.js
@@ -0,0 +1,35 @@
+/**
+ * Reverse the string
+ *
+ * @param {string} str
+ */
+const reverseString = (str) => {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+};
+
+/**
+ * Reverse string recursively
+ *
+ * @param {string} str
+ */
+const reverseStringRec = (str) => {
+  if (str.length === 0) return "";
+  return str[str.length - 1] + reverseString(str.slice(0, str.length - 1));
+};
+
+/**
+ * Reverse string using JS built in methods
+ *
+ * @param {string} str
+ */
+const reverseStringBuiltIn = (str) => str.split("").reverse().join("");
+
+module.exports = {
+  reverseString,
+  reverseStringRec,
+  reverseStringBuiltIn,
+};
---
+++ src/problems/reverseString.test.js
@@ -0,0 +1,23 @@
+const {
+  reverseString,
+  reverseStringRec,
+  reverseStringBuiltIn,
+} = require("./reverseString");
+
+test("To reverse the string", () => {
+  expect(reverseString("")).toBe("");
+  expect(reverseString("123")).toBe("321");
+  expect(reverseString("apple")).toBe("elppa");
+});
+
+test("To reverse the string recursively", () => {
+  expect(reverseStringRec("")).toBe("");
+  expect(reverseStringRec("123")).toBe("321");
+  expect(reverseStringRec("apple")).toBe("elppa");
+});
+
+test("To reverse the string using built-in JS methods", () => {
+  expect(reverseStringBuiltIn("")).toBe("");
+  expect(reverseStringBuiltIn("123")).toBe("321");
+  expect(reverseStringBuiltIn("apple")).toBe("elppa");
+});
