032828094d4bcbbbb04737144ec262e453f0fc48
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-09-07 13:52:27 +0500

    insertion sort

---
+++ src/sorting/insertionSort.js
@@ -0,0 +1,33 @@
+/**
+ * Sorts an array using insertion sort
+ *
+ * This has a complexity of O(n^2)
+ *
+ * This is also efficient for nearly sorted array O(n)
+ * Or when values are being continuously added / streamed (running sort)
+ *
+ * @param {number[]} arr
+ */
+const insertionSort = (arr) => {
+  // starting outer loop from the second element
+  // left hand side will always be sorted
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    // j loops from i all the way to 0
+    // it stops where value of j is greater the current value of outer loop
+    // this is the point to insert
+    for (var j = i - 1; j >= 0 && arr[j] > current; j--) {
+      // the inner loop moves all the value to right by making duplicates
+      // this makes space for where to insert the 'current value' in place of duplicate
+      arr[j + 1] = arr[j];
+    }
+    // insert current value where j stopped
+    // we need to refer j outside of its scope so var was used instead of let
+    arr[j + 1] = current;
+  }
+  return arr;
+};
+
+module.exports = {
+  insertionSort,
+};
---
+++ src/sorting/insertionSort.test.js
@@ -0,0 +1,12 @@
+const { insertionSort } = require("./insertionSort");
+
+test("Insertion Sort", () => {
+  expect(insertionSort([])).toEqual([]);
+  expect(insertionSort([4])).toEqual([4]);
+  expect(insertionSort([4, 300, 62])).toEqual([4, 62, 300]);
+  expect(insertionSort([37, 45, 29, 8])).toEqual([8, 29, 37, 45]);
+  expect(insertionSort([-3, -45])).toEqual([-45, -3]);
+  expect(insertionSort([1, 2, 3, 4])).toEqual([1, 2, 3, 4]); // last 2 to check if it is doing extra passes
+  expect(insertionSort([1, 3, 2, 4])).toEqual([1, 2, 3, 4]);
+  expect(insertionSort([10, 5, 5, 3])).toEqual([3, 5, 5, 10]);
+});
