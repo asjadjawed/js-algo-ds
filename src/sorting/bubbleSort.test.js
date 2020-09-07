547179a3c67e743c9ff54c7ed62c8e20d675b0e9
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-09-07 12:44:51 +0500

    repeating elements test

--- src/sorting/bubbleSort.test.js
+++ src/sorting/bubbleSort.test.js
@@ -8,4 +8,5 @@ test("Bubble Sort", () => {
   expect(bubbleSort([-3, -45])).toEqual([-45, -3]);
   expect(bubbleSort([1, 2, 3, 4])).toEqual([1, 2, 3, 4]); // last 2 to check if it is doing extra passes
   expect(bubbleSort([1, 3, 2, 4])).toEqual([1, 2, 3, 4]);
+  expect(bubbleSort([10, 5, 5, 3])).toEqual([3, 5, 5, 10]);
 });
