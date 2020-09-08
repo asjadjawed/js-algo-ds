8e5907acdcc96620928b4165352eec8c65f8a6e0
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-09-08 14:35:16 +0500

    welcome message

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-// const { countdown } = require("./recursion/countdown"); // recursion
-
 const check = () => "JestTest";
 
 const main = () => {
-  console.log("Happy Coding!");
-  // countdown(11292); // recursion (higher than 11292 will cause a stack overflow)
+  console.log("Happy Coding! Check the folders :)");
 };
 
 main();
