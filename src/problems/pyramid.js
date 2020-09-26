a775694dd3682fd4d4316333b5cd159e9d78d545
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-09-26 13:53:01 +0500

    pyramid maker

---
+++ src/problems/pyramid.js
@@ -0,0 +1,21 @@
+/**
+ * Makes a pyramid of height n
+ */
+const pyramid = (n) => {
+  const width = n * 2 - 1;
+  if (width < 1) {
+    console.log("");
+    return;
+  }
+
+  for (let i = 1; i <= width; i += 2) {
+    const floor = "#".repeat(i);
+    const gap = Math.floor(width - floor.length) / 2;
+    const step = " ".repeat(gap) + floor + " ".repeat(gap);
+    console.log(step);
+  }
+};
+
+module.exports = {
+  pyramid,
+};
