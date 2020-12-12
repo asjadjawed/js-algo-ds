ad938e7af899c080280445581b39ca61d7d46a37
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-12-12 05:55:22 +0500

    queue weaving

---
+++ src/problems/general/queueWeave.js
@@ -0,0 +1,20 @@
+const Queue = require("../../datastructures/queue/Queue");
+
+/**
+ * Weaves 2 queues
+ *
+ * @param {Queue} sourceOne
+ * @param {Queue} sourceTwo
+ */
+const weave = (sourceOne, sourceTwo) => {
+  const weavedQueue = new Queue();
+
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) weavedQueue.add(sourceOne.remove());
+    if (sourceTwo.peek()) weavedQueue.add(sourceTwo.remove());
+  }
+
+  return weavedQueue;
+};
+
+module.exports = { weave };
