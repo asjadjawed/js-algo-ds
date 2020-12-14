71d52ed45fe7014f9451c42fb8791da656ed8eff
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-12-14 09:23:21 +0500

    queue from stack

---
+++ src/problems/general/queueFromStack.js
@@ -0,0 +1,24 @@
+const Stack = require("../../datastructures/stack/Stack");
+
+class Queue {
+  constructor() {
+    this.queue = new Stack();
+    this.tempQueue = new Stack();
+  }
+
+  add(x) {
+    while (this.queue.peek()) this.tempQueue.add(this.queue.remove());
+    this.tempQueue.add(x);
+    while (this.tempQueue.peek()) this.queue.add(this.tempQueue.remove());
+  }
+
+  remove() {
+    return this.queue.remove();
+  }
+
+  peek() {
+    return this.queue.peek();
+  }
+}
+
+module.exports = Queue;
