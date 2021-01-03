bfad875036fc2c1865971820524d5358ae26db25
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2021-01-03 14:12:51 +0500

    tree width problem

---
+++ src/problems/general/treeLevelWidth.js
@@ -0,0 +1,22 @@
+const levelWidth = (node) => {
+  const L = "L";
+  const counter = [1, 0];
+  let q = [...node.children, L];
+
+  while (!(q.length === 1 && q[0] === L)) {
+    if (q[0] !== L) {
+      const current = q.shift();
+      ++counter[counter.length - 1];
+      q = [...q, ...current.children];
+    }
+
+    if (q[0] === L && q.length > 1) {
+      q.push(q.shift());
+      counter.push(0);
+    }
+  }
+
+  return counter;
+};
+
+module.exports = levelWidth;
