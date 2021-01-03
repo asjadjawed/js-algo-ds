d511641030bc3512e55445053cb10ac3a6348073
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2021-01-03 23:30:11 +0500

    check for same frequency

---
+++ src/problems/general/sameFrequency.js
@@ -0,0 +1,15 @@
+const sameFrequency = (n1, n2) => {
+  const arr1 = n1.toString().split("");
+  const arr2 = n2.toString().split("");
+  if (arr1.length !== arr2.length) return false;
+
+  for (const e of arr1) {
+    const checkIndex = arr2.indexOf(e);
+    if (checkIndex === -1) return false;
+    arr2.splice(checkIndex, 1);
+  }
+
+  return true;
+};
+
+module.exports = sameFrequency;
