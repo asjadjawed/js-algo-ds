1e2adeb7869e2efb75847f69219d02c6031c57a4
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2021-01-31 13:10:59 +0500

    grid travller problem

---
+++ src/patterns/dynamic/gridTraveler.test.js
@@ -0,0 +1,9 @@
+const gridTraveler = require("./gridTraveler");
+
+test("No of ways to travel down or right in a grid", () => {
+  expect(gridTraveler(1, 1)).toBe(1);
+  expect(gridTraveler(2, 3)).toBe(3);
+  expect(gridTraveler(3, 2)).toBe(3);
+  expect(gridTraveler(3, 3)).toBe(6);
+  expect(gridTraveler(18, 18)).toBe(2333606220);
+});
